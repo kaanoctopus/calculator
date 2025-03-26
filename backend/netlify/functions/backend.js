b7a85c9cb82c74fc58c956b1bded2d1e43bd3d04
kaanoctopus <kaan@octopussignage.com>
2025-03-26 13:10:07 +0300

    test

--- backend/netlify/functions/backend.js
+++ backend/netlify/functions/backend.js
@@ -7,6 +7,18 @@ const app = express();
 
 
 app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(204); // Important: End the response for preflight
+    }
+    
+    next();
+  });
 app.use(express.json());
 
 app.options("*", cors());
