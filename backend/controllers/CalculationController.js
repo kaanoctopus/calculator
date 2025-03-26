afcfdc662e8b3a83debd78fe29ca8b8e4ccfd1c5
kaanoctopus <kaan@octopussignage.com>
2025-03-26 11:17:43 +0300

    cors fix test

--- backend/controllers/CalculationController.js
+++ backend/controllers/CalculationController.js
@@ -1,19 +1,23 @@
 class CalculationController {
-    constructor(calculationService) {
-      this.calculationService = calculationService;
-      this.calculate = this.calculate.bind(this);
-    }
-  
-    calculate(req, res) {
-      const { expression } = req.body;
-      try {
-        const result = this.calculationService.evaluateExpression(expression);
-        res.json(result);
-      } catch (err) {
-        res.status(400).json({ error: err.message });
-      }
+  constructor(calculationService) {
+    this.calculationService = calculationService;
+    this.calculate = this.calculate.bind(this);
+  }
+
+  calculate(req, res) {
+    const { expression } = req.body;
+    try {
+      const result = this.calculationService.evaluateExpression(expression);
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+      res.json(result);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
     }
   }
-  
-  module.exports = CalculationController;
-  
+}
+
+module.exports = CalculationController;
