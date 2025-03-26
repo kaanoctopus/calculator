a6d03c45cc13a7e045fa7776637aef8605c0f9d4
kaanoctopus <kaan@octopussignage.com>
2025-03-26 12:41:26 +0300

    cors test

--- backend/netlify/functions/backend.js
+++ backend/netlify/functions/backend.js
@@ -6,16 +6,11 @@ const calculationRoutes = require('../../routes/CalculationRoutes');
 const app = express();
 
 
-app.use(
-    cors({
-      allowedHeaders: ["authorization", "Content-Type"], // you can change the headers
-      exposedHeaders: ["authorization"], // you can change the headers
-      origin: "*",
-      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-      preflightContinue: false
-    })
-  )
+app.use(cors());
 app.use(express.json());
+
+app.options("*", cors());
+
 app.use('/api/', calculationRoutes);
 
 module.exports.handler = serverless(app);
