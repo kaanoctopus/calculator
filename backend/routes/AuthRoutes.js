50e5e8fcd1a6de48eeac0632d7f352ea970971de
kaanoctopus <kaan@octopussignage.com>
2025-03-27 12:41:54 +0300

    test

--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -2,11 +2,15 @@ const express = require("express");
 const AuthService = require("../services/AuthService");
 const AuthController = require("../controllers/AuthController");
 const authMiddleware = require("../middlewares/AuthMiddleware");
+const cors = require("cors");
+
 
 const router = express.Router();
 
 const authService = new AuthService();
 const authController = new AuthController(authService);
+
+router.use(cors());
 
 router.options("*", (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -16,7 +20,7 @@ router.options("*", (req, res) => {
 });
 
 router.post("/register", authController.register);
-router.post("/login", authMiddleware, authController.login);
+router.post("/login", authController.login);
 router.get("/me", authMiddleware, authController.getMe);
 router.put("/me", authMiddleware, authController.updateProfile);
 router.delete("/me", authMiddleware, authController.deleteAccount);
