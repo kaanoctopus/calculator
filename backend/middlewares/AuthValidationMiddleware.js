b2d1683a936fc5a753c7ef5e600d2ff12bccc876
kaanoctopus <kaan@octopussignage.com>
2025-04-07 21:41:05 +0300

    password length validation fix

--- backend/middlewares/AuthValidationMiddleware.js
+++ backend/middlewares/AuthValidationMiddleware.js
@@ -5,7 +5,7 @@ const registerValidation = [
   body("lastName").trim().notEmpty().withMessage("Last name is required"),
   body("email").trim().isEmail().withMessage("Valid email is required"),
   body("password")
-    .isLength({ min: 8 }).withMessage("Password must be at least 8 characters")
+    .isLength({ min: 6 }).withMessage("Password must be at least 6 characters")
     .matches(/[A-Z]/).withMessage("Password must contain at least one uppercase letter")
     .matches(/[a-z]/).withMessage("Password must contain at least one lowercase letter")
     .matches(/[0-9]/).withMessage("Password must contain at least one number")
@@ -29,7 +29,7 @@ const forgotPasswordValidation = [
 const resetPasswordValidation = [
   body("token").notEmpty().withMessage("Token is required"),
   body("newPassword")
-    .isLength({ min: 8 }).withMessage("Password must be at least 8 characters")
+    .isLength({ min: 6 }).withMessage("Password must be at least 6 characters")
     .matches(/[A-Z]/).withMessage("Password must contain at least one uppercase letter")
     .matches(/[a-z]/).withMessage("Password must contain at least one lowercase letter")
     .matches(/[0-9]/).withMessage("Password must contain at least one number")
--- backend/routes/AuthRoutes.js
+++ backend/routes/AuthRoutes.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const { registerValidation, loginValidation, updateProfileValidation, forgotPasswordValidation, resetPasswordValidation } = require("../middlewares/AuthValidationMiddleware");
+const {
+    registerValidation,
+    loginValidation,
+    updateProfileValidation,
+    forgotPasswordValidation,
+    resetPasswordValidation,
+} = require("../middlewares/AuthValidationMiddleware");
 const AuthService = require("../services/AuthService");
 const AuthController = require("../controllers/AuthController");
 const authMiddleware = require("../middlewares/AuthMiddleware");
@@ -15,25 +21,45 @@ router.use(helmet());
 router.use(cors());
 
 const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  message: "Too many requests from this IP, please try again later"
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: "Too many requests from this IP, please try again later",
 });
 
 router.options("*", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.sendStatus(204);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
+    res.sendStatus(204);
 });
 
-router.post("/register", [authLimiter, ...registerValidation], authController.register);
+router.post(
+    "/register",
+    [authLimiter, ...registerValidation],
+    authController.register
+);
 router.post("/login", [authLimiter, ...loginValidation], authController.login);
 router.get("/me", authMiddleware, authController.getMe);
-router.put("/me", authMiddleware, updateProfileValidation, authController.updateProfile);
+router.put(
+    "/me",
+    authMiddleware,
+    updateProfileValidation,
+    authController.updateProfile
+);
 router.delete("/me", authMiddleware, authController.deleteAccount);
 router.post("/logout", authMiddleware, authController.logout);
-router.post("/forgot-password", [authLimiter, ...forgotPasswordValidation], authController.forgotPassword);
-router.post("/reset-password", [authLimiter, ...resetPasswordValidation], authController.resetPassword);
+router.post(
+    "/forgot-password",
+    [authLimiter, ...forgotPasswordValidation],
+    authController.forgotPassword
+);
+router.post(
+    "/reset-password",
+    [authLimiter, ...resetPasswordValidation],
+    authController.resetPassword
+);
 
 module.exports = router;
